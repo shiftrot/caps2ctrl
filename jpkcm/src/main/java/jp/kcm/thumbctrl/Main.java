51fcedd0873497c53c287184feecbfb8d8f8988d
shiftrot <shiftrot@gmail.com>
2019-03-07 00:01:07 +0900

    Improve Webview

--- jpkcm/src/main/java/jp/kcm/thumbctrl/Main.java
+++ jpkcm/src/main/java/jp/kcm/thumbctrl/Main.java
@@ -2,11 +2,15 @@ package jp.kcm.thumbctrl;
 
 import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -19,6 +23,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main extends AppCompatActivity {
 
@@ -29,6 +35,7 @@ public class Main extends AppCompatActivity {
     private WebView mWebView;
     private PrefValue mPrefValue;
     private final String PREFKEY_FONTSIZE = "fontsize";
+    private Handler mHandler = new Handler();
     @SuppressLint({"SetJavaScriptEnabled", "NewApi"})
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -96,6 +103,9 @@ public class Main extends AppCompatActivity {
         findViewById(R.id.webview_forward).setOnClickListener(mButtonListener);
         findViewById(R.id.webview_reload).setOnClickListener(mButtonListener);
         findViewById(R.id.webview_abort).setOnClickListener(mButtonListener);
+        findViewById(R.id.webview_top).setOnClickListener(mButtonListener);
+        findViewById(R.id.webview_bottom).setOnClickListener(mButtonListener);
+        findViewById(R.id.webview_menu).setOnClickListener(mButtonListener);
         findViewById(R.id.webview_quit).setOnClickListener(mButtonListener);
         findViewById(R.id.webview_plus).setOnTouchListener(new RepeatListener(mInitialInterval, mNormalInterval, mButtonListener));
         findViewById(R.id.webview_minus).setOnTouchListener(new RepeatListener(mInitialInterval, mNormalInterval, mButtonListener));
@@ -119,16 +129,6 @@ public class Main extends AppCompatActivity {
                         mWebView.goForward();
                     }
                     break;
-                case R.id.webview_reload:
-                    mWebView.reload();
-                    break;
-                case R.id.webview_abort:
-                    mWebView.stopLoading();
-                    break;
-                case R.id.webview_quit:
-                    mWebView.stopLoading();
-                    finish();
-                    break;
                 case R.id.webview_plus:
                     mWebView.getSettings().setTextZoom(mWebView.getSettings().getTextZoom() + 10);
                     mFontSize = mWebView.getSettings().getTextZoom();
@@ -139,11 +139,73 @@ public class Main extends AppCompatActivity {
                     mFontSize = mWebView.getSettings().getTextZoom();
                     mPrefValue.setInt(PREFKEY_FONTSIZE, mFontSize);
                     break;
+                case R.id.webview_reload:
+                    mWebView.reload();
+                    break;
+                case R.id.webview_abort:
+                    mWebView.stopLoading();
+                    break;
+                case R.id.webview_top:
+                    mWebView.scrollTo(0,0);
+                    break;
+                case R.id.webview_bottom:
+                    mWebView.scrollTo(0,mWebView.getContentHeight());
+                    break;
+                case R.id.webview_quit:
+                    confirmQuit();
+                    break;
+                case R.id.webview_menu:
+                    menu();
+                    break;
                 default:
                     break;
             }
         }
     };
+
+    final Runnable mFinish = new Runnable() {
+        public void run() {
+            mWebView.stopLoading();
+            finish();
+        }
+    };
+
+    private void confirmQuit() {
+        final AlertDialog.Builder b = new AlertDialog.Builder(this);
+        b.setIcon(android.R.drawable.ic_dialog_alert);
+        b.setMessage(R.string.query_quit);
+        b.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+           public void onClick(DialogInterface dialog, int id) {
+               dialog.dismiss();
+               mHandler.post(mFinish);
+           }
+        });
+        b.setNegativeButton(android.R.string.no, null);
+        b.show();
+    }
+
+    private void menu() {
+        String[] items = new String[]{getString(R.string.scroll_to_top),
+                                      getString(R.string.scroll_to_bottom),
+                                      getString(R.string.quit)};
+        final int quit = items.length-1;
+        new AlertDialog.Builder(mWebView.getContext())
+                .setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if (which == quit) {
+                            mHandler.post(mFinish);
+                        } else if (which == 0) {
+                            mWebView.scrollTo(0,0);
+                        } else if (which == 1) {
+                            mWebView.scrollTo(0, mWebView.getContentHeight());
+                        }
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .show();
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -196,40 +258,6 @@ public class Main extends AppCompatActivity {
         return true;
     }
 
-    private boolean openSettings(String url) {
-        if (!url.matches("file:///android_asset/ACTION_SETTINGS.*")) {
-            return false;
-        }
-
-        if (url.matches("file:///android_asset/ACTION_SETTINGS.LanguageAndInputSettings")) {
-            Intent intent = new Intent();
-            intent.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$InputMethodAndLanguageSettingsActivity" ));
-            if (startIntent(intent)) {
-                intent = new Intent();
-                intent.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$LanguageAndInputSettingsActivity" ));
-                if (startIntent(intent)) {
-                    intent = new Intent(Settings.ACTION_SETTINGS);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startIntent(intent);
-                }
-            }
-        } else {
-            Intent intent = new Intent(Settings.ACTION_SETTINGS);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startIntent(intent);
-        }
-        return true;
-    }
-
-    private boolean startIntent(Intent intent) {
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            return true;
-        }
-        return false;
-    }
-
     private boolean openPlayStorePage(String url) {
         if (!url.matches("https?://play.google.com/store/apps/details\\?id=.*")) {
             return false;
@@ -245,6 +273,51 @@ public class Main extends AppCompatActivity {
         } catch (ActivityNotFoundException e) {
             startActivity(new Intent(Intent.ACTION_VIEW,
                     Uri.parse(url)));
+        }
+        return true;
+    }
+
+    /*
+     *  Open Android Settings
+     *  If there is a link starting with "file:///android_asset/ACTION_SETTINGS/", open the corresponding setting screen.
+     *  E.g. <a href="file:///android_asset/ACTION_SETTINGS/ACTION_HARD_KEYBOARD_SETTINGS">Physical Keyboard settings&</a>
+     */
+    static final private Map<String, String> mSettingsAction = new HashMap<String, String>() {
+        {
+            put("ACTION_SETTINGS", Settings.ACTION_SETTINGS);
+            put("ACTION_INPUT_METHOD_SETTINGS", Settings.ACTION_INPUT_METHOD_SETTINGS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                put("ACTION_HARD_KEYBOARD_SETTINGS", Settings.ACTION_HARD_KEYBOARD_SETTINGS);
+            }
+        }
+    };
+
+    private boolean openSettings(String url) {
+        final String ANDROID_SETTINGS = "file:///android_asset/ACTION_SETTINGS";
+        if (!url.startsWith(ANDROID_SETTINGS)) {
+            return false;
+        }
+        url = url.replaceFirst(ANDROID_SETTINGS+"/?", "");
+        String action = getSettingsAction(url);
+        Intent intent = new Intent(action);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return startIntent(intent);
+    }
+
+    private String getSettingsAction(String action) {
+        String settingsAction = Settings.ACTION_SETTINGS;
+        if ((action.equals("ACTION_HARD_KEYBOARD_SETTINGS") && (Build.VERSION.SDK_INT < Build.VERSION_CODES.N))) {
+            action = "ACTION_INPUT_METHOD_SETTINGS";
+        }
+        if (mSettingsAction.containsKey(action)) return mSettingsAction.get(action);
+        return settingsAction;
+    }
+
+    private boolean startIntent(Intent intent) {
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            return false;
         }
         return true;
     }
